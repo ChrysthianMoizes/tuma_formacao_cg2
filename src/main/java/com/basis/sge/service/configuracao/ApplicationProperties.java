247abbe60da22094f78314edf9b1c0b1bfc5506f
Chrysthian Moizes <chrysthian.moizes@basis.com.br>
2021-01-19 15:50:25 -0300

    Implementaçao de fila

--- src/main/java/com/basis/sge/service/configuracao/ApplicationProperties.java
+++ src/main/java/com/basis/sge/service/configuracao/ApplicationProperties.java
@@ -1,12 +1,14 @@
 package com.basis.sge.service.configuracao;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix = "application.mail")
 @Configuration
-@Data
+@Getter
+@Setter
 public class ApplicationProperties {
 
     private String enderecoRemetente;
--- src/main/java/com/basis/sge/service/servico/UsuarioServico.java
+++ src/main/java/com/basis/sge/service/servico/UsuarioServico.java
@@ -2,6 +2,7 @@ package com.basis.sge.service.servico;
 
 import com.basis.sge.service.dominio.Usuario;
 import com.basis.sge.service.repositorio.UsuarioRepositorio;
+import com.basis.sge.service.servico.dto.EmailDTO;
 import com.basis.sge.service.servico.dto.UsuarioDTO;
 import com.basis.sge.service.servico.exception.RegraNegocioException;
 import com.basis.sge.service.servico.mapper.UsuarioMapper;
@@ -19,6 +20,7 @@ public class UsuarioServico {
 
     private final UsuarioRepositorio usuarioRepositorio;
     private final UsuarioMapper usuarioMapper;
+    private final ProdutorServico produtorServico;
 
     public List<UsuarioDTO> listar() {
         List<Usuario> usuarios = usuarioRepositorio.findAll();
@@ -35,14 +37,50 @@ public class UsuarioServico {
                 .orElseThrow(() -> new RegraNegocioException("Usuário não encontrado"));
     }
 
+    private void validarEmail(UsuarioDTO usuarioDTO) {
+        Usuario usuario = usuarioRepositorio.findByEmail(usuarioDTO.getEmail());
+        if(usuario != null && !usuario.getId().equals(usuarioDTO.getId())) {
+            throw new RegraNegocioException("Email já cadastrado");
+        }
+    }
+
+    private void validarCpf(UsuarioDTO usuarioDTO) {
+        Usuario usuario = usuarioRepositorio.findByCpf(usuarioDTO.getCpf());
+        if(usuario != null && !usuario.getId().equals(usuarioDTO.getId())) {
+            throw new RegraNegocioException("CPF já cadastrado");
+        }
+    }
+
     public UsuarioDTO salvar(UsuarioDTO usuarioDTO) {
+
+        validarEmail(usuarioDTO);
+        validarCpf(usuarioDTO);
+
         Usuario usuario = usuarioMapper.toEntity(usuarioDTO);
         usuario.setChave(UUID.randomUUID().toString());
         usuarioRepositorio.save(usuario);
+
+        EmailDTO emailDTO = gerarEmailCriarUsuario(usuario);
+        produtorServico.enviarEmail(emailDTO);
+
         return usuarioMapper.toDto(usuario);
     }
 
+    private EmailDTO gerarEmailCriarUsuario(Usuario usuario) {
+        EmailDTO emailDTO = new EmailDTO();
+
+        emailDTO.setAssunto("Cadastro de Usuário");
+        emailDTO.setCorpo("Você se cadastrou com sucesso na plataforma de eventos, esta é sua chave de inscrição aos eventos: <b>" + usuario.getChave() + "</b>");
+        emailDTO.setDestinatario(usuario.getEmail());
+
+        return emailDTO;
+    }
+
     public UsuarioDTO editar(UsuarioDTO usuarioDTO) {
+
+        validarEmail(usuarioDTO);
+        validarCpf(usuarioDTO);
+
         Usuario usuarioSalvo = obter(usuarioDTO.getId());
         Usuario usuario = usuarioMapper.toEntity(usuarioDTO);
         usuario.setChave(usuarioSalvo.getChave());
